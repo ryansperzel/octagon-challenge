ec986095242af1c20513ac120376f08b8a034a45
Ryan Sperzel <ryansperzel@gmail.com>
2018-01-05 19:36:23 -0500

    Set up basic NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 export default class NavBar extends Component {
 
   render() {
     return (
       <div className="Nav">
-
-
-          <div><NavLink to="/"> Home </NavLink></div>
-
-          <div><NavLink to="/budgets"> Budgets </NavLink></div>
-
-
+        <Navbar className="main-head" inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Nav pullRight>
+            <IndexLinkContainer to="/">
+              <NavItem className="link" eventKey={1}>HOME</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/contact">
+              <NavItem className="link" eventKey={2}>CONTACT</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
       </div>
     )
   }
