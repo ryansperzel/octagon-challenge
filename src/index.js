95f7d07436a79f754c3970819f059b76382e1e7f
Ryan Sperzel <ryansperzel@gmail.com>
2018-01-15 20:42:37 -0500

    Added redux thunk library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
